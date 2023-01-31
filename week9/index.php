83a8fbd6f9eab7d2b483767fb60625f8b1661097
rtgenaapc <rtgena@apc.edu.ph>
2023-01-31 09:12:32 +0800

    PHP update to files

---
+++ week9/index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>REINER GENA</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="mystyle.css">
+              <script src="./message.js"></script>
+  </head>
+    <body>
+      <div class="header">
+        <h1>RJ.</h1>
+        <div class="header-right">
+            <a href="resource.html"><B>RESOURCES</B></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="column1">    
+        
+         <p>My name is Reiner Gena but you can call me <b>RJ.</b> I'm currently <b>20 Years Old</b> from <b>Makati City</b>, and currently an undergraduate under
+            Bachelor of Science in Information Technology with specialiation in Mobile and internet technologies. 
+            <br><br>
+            I work on different student organizations such as <b>MSC, 
+            JPCS, ROTARACT, AND BAHAY BOMBILYA.</b>
+            <br><br>
+            <b>OPO!!!!!! TINATAMAD PO AKO MAGCODE SA SCHOOL MAN O BAKASYON</b>
+          </p>
+        </div>
+        <div class="column2">
+        <img src="https://i.pinimg.com/564x/b1/23/38/b12338d5b6108be01f7c8cb9dd5497e7.jpg" 
+        alt="REINER SELFIE" width="400" height="300"  style="width: 400px; margin-left: 250px; position: static; border-radius: 20%" >
+        </div>
+      </div>
+      <div class ="row2" style="background-color: beige;">
+
+          <button type="button" 
+          onclick="document.getElementById('demo').innerHTML = Date()">
+          <b>WHAT'S THE TIME AND DATE ???? </b></button>
+          <p id="demo" style="font-weight: bold;"></p>
+      </div>
+      <div class="Table">
+        <h1 style="text-align: center;">know more about me?</h1>
+        <table>
+           <tr>
+             <th>Other Nicknames?</th>
+             <th>Favorite Colors?</th>
+             <th>KPOP Bias?</th>
+           </tr>
+           <tr>
+             <td>Jay</td>
+             <td>BLACKPINK</td>
+             <td>Lisa</td>
+           </tr>
+           <tr>
+             <td>RAY-RAY</td>
+             <td>TWICE</td>
+             <td>Momo</td>
+           </tr>
+           <tr>
+            <td>Reindeer</td>
+            <td>ITZY</td>
+            <td>Ryugin</td>
+           </tr>
+           <tr>
+            <td>Baby : ></td>
+            <td>XG</td>
+            <td>Cocona</td>
+           </tr>
+        </table>
+
+      </div>
+      <div class="JS_prac">
+            <div class="JS_prac1" style="margin-bottom: 20px;">
+             <h1 id="myH"></h1>
+               <p id="myP"></p>
+  
+            <script>
+  
+              document.getElementById("myH").innerHTML = "I'M USING JAVASCRIPT HERE : >";
+              document.getElementById("myP").innerHTML = " WANNA DO ONE MORE?";
+            </script>
+            <br><br><br>
+        </div>
+        <div class="JS_prac2">
+           <h2>let's try JavaScript Variables</h2>
+            <!--LESSON 2.1 VARIABLES-->
+            <p>In this example, x = 11 , y = 26, and z are variables.</p> <br><br><br>
+            <p id="myAnswer"></p>
+
+           <script>
+             let x = 11;
+             let y = 26;
+             let z = x + y;
+    
+              document.getElementById("myAnswer").innerHTML = "The value of z is: " + z;
+           </script>
+        </div>
+        <div class="JS_prac3">
+          <br><br><br>
+          <h1>JavaScript Variables</h1>
+          <p>Strings are written with quotes.</p>
+          <br><br><br>
+          <p>Numbers are written without quotes.</p>
+          <br><br><br>
+          <p id="MYVAB"></p>
+
+          <script>
+            const pi = 3.14;
+            let person = "REINER GENA";
+            let answer = 'Yes I am!';
+
+            document.getElementById("MYVAB").innerHTML =
+            pi + "<br>" + person + "<br>" + answer;
+          </script>
+
+        </div>
+        <div class="JS_prac4">
+          <br><br><br><br><br>
+          <h1>JavaScript Arithmetic</h1>
+          <h2>Arithmetic Operations</h2>
+          <p>A typical arithmetic operation takes two numbers (or expressions) and produces a new number.</p>
+          <br><br><br>
+          <p id="arith"></p>
+            <!--LESSON 5.1 OPERATORS-->
+            <!--LESSON 5.3 ARITHMETIC-->
+          <script>
+            let a = 25;
+            let c = (100 + 50) * a;
+            document.getElementById("arith").innerHTML = c;
+          </script>
+        </div>
+        <div class="JS_prac5">
+          <br><br><br>
+          <h2>JavaScript Map Objects</h2>
+          <p>Creating a Map from an Array:</p>
+          <br><br><br>
+          <p id="mapp"></p>
+          <br><br><br>
+          <!--LESSON 11.1 MAPS-->
+            <script>
+              const fruits = new Map([["apples", 500],["bananas", 300],["oranges", 200]]);
+      
+              document.getElementById("mapp").innerHTML = fruits.get("apples");
+            </script>
+        </div>
+        <div class="JS_prac6">
+          <h2>JavaScript Objects</h2>
+          <p>Object.values() converts an object to an array.</p>
+          <br><br><br>
+          <p id="objval"></p>
+           <!--LESSON 17.5 OBJECT VALUE-->
+
+            <script>
+              const tao = { name: "Bryan",age: 18,city: "Makati"};
+
+              document.getElementById("objval").innerHTML = Object.values(tao);
+            </script>
+        </div>
+        <div class="JS_prac7">
+         <br><br><br>
+         <h1>JavaScript Modules</h1>
+         <br><br><br>
+         <p id="mod"></p>
+  
+          <script type="module">
+              import message from "./message.js";
+  
+              document.getElementById("mod").innerHTML = message();
+  
+          </script>
+          <!--LESSON 19 MODULES-->
+          <!--LESSON 22.2 DOM METHOD-->
+        </div>
+        <div class="JS_prac8">
+          <br><br><br>
+          <h1>Windows Screen</h1>
+          <p id="WS"></p>
+          <!--LESSON 23.3 Windows Screen-->
+            <script>
+              document.getElementById("WS").innerHTML = 
+              "Screen height is " + screen.height;
+
+            </script>
+        </div>
+        <div class="JS_prac9">
+          <br><br><br>
+          <h1>Plotly.js</h1>
+          <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+          <body>
+
+          <div id="myPlot" style="width:100%;max-width:700px"></div>
+
+          <script>
+            var xArray = [7,8,9,10,11,12,13,14,];
+            var yArray = [2,1,1,2,5,1,1,2];
+
+            // Define Data
+            var data = [{
+            x:xArray,
+            y:yArray,
+            mode:"markers"
+            }];
+
+            // Define Layout
+            var layout = {
+            xaxis: {range: [5, 15], title: "NUMBER OF CRUSHES"},
+            yaxis: {range: [0, 8], title: "YEAR LEVEL"},  
+            title: "CRUSH EME"
+            };
+
+            // Display using Plotly
+            Plotly.newPlot("myPlot", data, layout);
+            </script>
+            <!--LESSON 28.4 PLOTLY-->
+            </div>
+            <div class="JS_image">
+              <button onclick="document.getElementById('myImage').src='images/gal_friends.jpg'">FRIENDS</button>
+              <button onclick="document.getElementById('myImage').src='images/gal_org.jpg'">SCHOOL ORG</button>
+              <button onclick="document.getElementById('myImage').src='images/gal_orgfair.jpg'">ORG FAIR</button>
+              <button onclick="document.getElementById('myImage').src='images/gal_photoshoot.jpg'">PHOTOSHOOT</button>
+              <br>
+              <img id="myImage" src="images/gal_selfie.jpg" style="width:100px">
+            </div>
+<!-- ENDING OF THE MAIN BODY-->
+</div>
+    <footer>© 2022 Reiner Gena.</footer>
+  </body>
+</html>
